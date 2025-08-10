8e770a0ff0d5e6bc405fe57f40f41e8222334e3d
LiviaJanke <eichmliv12@gmail.com>
2025-08-10 16:00:30 +0100

    Session One reading and writing files

--- SessionOneImperial.py
+++ SessionOneImperial.py
@@ -10,18 +10,63 @@ import pandas as pd
 
 #%%
 
-# Different ways of reading data into numpy
-# numoy loadtxt - most basic,
+# Datetime 
+# Use standard ISOformat of YYYY-MM-DD where possible 
 
-resistivity_data_txt = np.loadtxt('Resistivity.txt', usecols = 1, skiprows = 5)
+from datetime import date
+from datetime import timedelta 
+
+#%%
+
+# Different ways of reading data into numpy
+# numpy loadtxt - most basic,
+
+resistivity_data_txt = np.loadtxt('C:/Users/Test/Richard_coding/RichardCoding/Resistivity.txt')
 # newest version assumes whitespace
+
+# Can specify whole path name with forward slashes 
+# Or can use os.path to add directories to search - but try to avoid this!
+
+#%%
+
+# numpy genfromtxt - slightly more sophisticated
+
+resistivity_data_txt_v2 = np.genfromtxt('Resistivity.txt')
+
 
 #%%
 
 
+# pandas read_csv - good for large data sets
 
-resistivity_data_txt_v2 = np.genfromtxt('Resistivity.txt')
+resistivity_df = pd.read_csv('Resistivity.csv', names = ['Temperature (K)', 
+                                                         'Resistivity Cu Ohm m', 
+                                                         'Resistivity Al Ohm m'], 
+                             skiprows = 2)
 
+
+# To select a column from a pandas dataframe, use the column name
+
+df_temperatures = resistivity_df['Temperature (K)']
+
+#%%
+
+# Read in Car_Data. csv using the three methods discussed:
+
+car_data_loadtxt = np.loadtxt('C:/Users/Test/Richard_coding/RichardCoding/Car_Data.csv', 
+                              skiprows = 1, delimiter = ',', dtype = str)
+
+#%%
+
+car_data_genfromtxt = np.genfromtxt('Car_Data.csv', skip_header = 1, delimiter = ',', dtype = str)
+
+#%% 
+
+car_data_df = pd.read_csv('Car_Data.csv')
+
+#%%
+
+np.savetxt('saved data.txt', df_temperatures)
 
 
 
